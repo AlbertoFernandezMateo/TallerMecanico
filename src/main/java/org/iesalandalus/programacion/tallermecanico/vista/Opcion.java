18000da71ba2bc14300ab0f1977444930fa99984
nombre del usuario 2 <usuario2@example.com>
2026-03-25 13:59:21 +0100

    Taller mecanico ALberto Fernandez Mateo

--- src/main/java/org/iesalandalus/programacion/tallermecanico/vista/Opcion.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/vista/Opcion.java
@@ -1,4 +1,4 @@
-package org.iesalandalus.programacion.tallermecanico.modelo.vista;
+package org.iesalandalus.programacion.tallermecanico.vista;
 
 public enum Opcion {
     INSERTAR_CLIENTE,
--- src/test/java/org/iesalandalus/programacion/tallermecanico/modelo/vista/Vista.java
+++
@@ -1,4 +0,0 @@
-package org.iesalandalus.programacion.tallermecanico.modelo.vista;
-
-public class Vista {
-}
